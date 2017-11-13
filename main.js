7ad20894e80c718ccaf1274a9092f9e2fe80cab9
ppoggi <ppoggi92@gmail.com>
2017-11-12 22:02:34 -0500

    Navigating to links gathered
    Links that are gathered are navigated to and the status is printed out

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//Patrick Poggi
+
+//This is a small script that retrieves all the links from the general info
+//section of the UnrealEngine FAQ page, navigates to the links and ensures
+// there are no 404's
+
+
+
 //headless browser library
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
@@ -5,21 +13,14 @@ const nightmare = Nightmare({ show: false });
 // great utility library
 const _ = require('underscore');
 
-// server side jquery library
-//not used, but just incase....
-const cheerio = require('cheerio')
-
-//cheerio example
-//const $ = cheerio.load('<h2 class="title">Hello world</h2>')
-
 //URL being tested
 let promise = new Promise((resolve, reject)=>{
   nightmare.goto('https://www.unrealengine.com/en-US/faq')
-  .wait(10000).evaluate(() =>{
+  .wait('body').evaluate(() =>{
 
     let linkArray = [];
     //Get all the Links on the page
-    let linkSelector = document.querySelectorAll('a');
+    let linkSelector = document.querySelectorAll('#generalInfo a');
     //push links to array
     for(let i=0;i<linkSelector.length; i++){
       linkArray.push(linkSelector[i].href);
@@ -27,7 +28,7 @@ let promise = new Promise((resolve, reject)=>{
     //send links to node
     return linkArray;
   })
-  .end()
+
   .then(function(){
     // links resolved in the promise
     resolve(arguments[0]);
@@ -40,22 +41,43 @@ let promise = new Promise((resolve, reject)=>{
 // resolution
 promise.then((links)=>{
 
-  //comparing length of links list to the list after falsey values have been removed
-  let preLength = links.length;
-  let linksCompact = _.compact(links);
-  let postLength = linksCompact.length;
+   //remove cdn links
+    links = removeCDN(links);
+    //comparing length of links list to the list after falsey values have been removed
+    let preLength = links.length;
+    let linksCompact = _.compact(links);
+    let postLength = linksCompact.length;
+    //if postLength <  preLength there are dead links
+    if(postLength < preLength){
+      console.error('Failure:', 'Dead Links on the page')
+    }
+    //remove and duplicate links
+    let linksParsed = _.uniq(links);
+    //go through all the links and see what the results are
+    let promises = [];
+    for(let i=0; i< linksParsed.length; i++){
+      //navigate to earch link and print out what the status is
+          nightmare.goto(linksParsed[i]).then(function(){
+            console.log('site:',linksParsed[i],'\ncode:', arguments[0].code)
+            //TODO make sure 404's are handled correctly
+          }
+        )
+     }
+}, (err)=>{
+  console.log(err)
+    }
+    //not working correclty...ideally should close out when all the urls have
+    //been navigated to
+  ).then(()=>{
+    nightmare.halt(err, ()=>{console.log('nightmare successfully closed')})
+  })
 
-  //if postLength <  preLength there are dead links
-  if(postLength > preLength){
-    console.error('Failure:', 'Dead Links on the page')
-  }
-  //remove and duplicate links
-  let linksParsed = _.uniq(links);
+//function to remove any cdn links
+function removeCDN(array){
 
-  for(let i=0; i< linksParsed.length; i++){
-    //goto links using whichever api wanted/needed
-    //console.log(i," " + linksParsed[i]);
-    //nightmare.goto().end().then().catch((error)=>{
-      //console.error('Failure:', error);
+  for(let i = 0; i<array.length; i++){
+    if(array[i].includes("cdn"))
+      array[i] = '';
   }
-});
+  return array;
+}
