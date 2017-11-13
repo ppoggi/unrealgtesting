0cec0e8d7aeeb2d952470d6abda5f3b8a6d29c1e
ppoggi <ppoggi92@gmail.com>
2017-11-13 10:14:33 -0500

    Edited false value check
    removed unreliable false value check

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-//Patrick Poggi
-
+//Author:Patrick Poggi
 //This is a small script that retrieves all the links from the general info
 //section of the UnrealEngine FAQ page, navigates to the links and ensures
 // there are no 404's
-
-
-
 //headless browser library
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
-
 // great utility library
 const _ = require('underscore');
-
 //URL being tested
 let promise = new Promise((resolve, reject)=>{
   nightmare.goto('https://www.unrealengine.com/en-US/faq')
@@ -28,7 +22,6 @@ let promise = new Promise((resolve, reject)=>{
     //send links to node
     return linkArray;
   })
-
   .then(function(){
     // links resolved in the promise
     resolve(arguments[0]);
@@ -40,17 +33,10 @@ let promise = new Promise((resolve, reject)=>{
 });
 // resolution
 promise.then((links)=>{
-
    //remove cdn links
     links = removeCDN(links);
-    //comparing length of links list to the list after falsey values have been removed
-    let preLength = links.length;
+    //remove falsey values
     let linksCompact = _.compact(links);
-    let postLength = linksCompact.length;
-    //if postLength <  preLength there are dead links
-    if(postLength < preLength){
-      console.error('Failure:', 'Dead Links on the page')
-    }
     //remove and duplicate links
     let linksParsed = _.uniq(links);
     //go through all the links and see what the results are
@@ -71,10 +57,8 @@ promise.then((links)=>{
   ).then(()=>{
     nightmare.halt(err, ()=>{console.log('nightmare successfully closed')})
   })
-
-//function to remove any cdn links
+//helper function to remove any cdn links
 function removeCDN(array){
-
   for(let i = 0; i<array.length; i++){
     if(array[i].includes("cdn"))
       array[i] = '';
